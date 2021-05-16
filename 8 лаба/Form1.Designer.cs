638f0e0fcca762bef7175675169d9a40c86e4401
NikitaAxenov <64533263+NikitaAxenov@users.noreply.github.com>
2021-05-16 17:11:26 +0300

    Сделано

---
+++ 8 лаба/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace _8_лаба
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yesOrNoLabel = new System.Windows.Forms.Label();
+            this.predictionLabel = new System.Windows.Forms.Label();
+            this.yesOrNoBtn = new System.Windows.Forms.Button();
+            this.predictionBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // yesOrNoLabel
+            // 
+            this.yesOrNoLabel.AutoSize = true;
+            this.yesOrNoLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.yesOrNoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.yesOrNoLabel.Location = new System.Drawing.Point(350, 31);
+            this.yesOrNoLabel.Name = "yesOrNoLabel";
+            this.yesOrNoLabel.Size = new System.Drawing.Size(88, 31);
+            this.yesOrNoLabel.TabIndex = 0;
+            this.yesOrNoLabel.Text = "Ответ";
+            this.yesOrNoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // predictionLabel
+            // 
+            this.predictionLabel.AutoSize = true;
+            this.predictionLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.predictionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.predictionLabel.Location = new System.Drawing.Point(350, 164);
+            this.predictionLabel.Name = "predictionLabel";
+            this.predictionLabel.Size = new System.Drawing.Size(88, 31);
+            this.predictionLabel.TabIndex = 1;
+            this.predictionLabel.Text = "Ответ";
+            this.predictionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // yesOrNoBtn
+            // 
+            this.yesOrNoBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.yesOrNoBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.yesOrNoBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.yesOrNoBtn.Location = new System.Drawing.Point(25, 23);
+            this.yesOrNoBtn.Name = "yesOrNoBtn";
+            this.yesOrNoBtn.Size = new System.Drawing.Size(250, 45);
+            this.yesOrNoBtn.TabIndex = 2;
+            this.yesOrNoBtn.Text = "Да или Нет";
+            this.yesOrNoBtn.UseVisualStyleBackColor = false;
+            this.yesOrNoBtn.Click += new System.EventHandler(this.yesOrNoBtn_Click);
+            // 
+            // predictionBtn
+            // 
+            this.predictionBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.predictionBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.predictionBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.predictionBtn.Location = new System.Drawing.Point(25, 156);
+            this.predictionBtn.Name = "predictionBtn";
+            this.predictionBtn.Size = new System.Drawing.Size(288, 45);
+            this.predictionBtn.TabIndex = 3;
+            this.predictionBtn.Text = "Предсказание";
+            this.predictionBtn.UseVisualStyleBackColor = false;
+            this.predictionBtn.Click += new System.EventHandler(this.predictionBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1358, 450);
+            this.Controls.Add(this.predictionBtn);
+            this.Controls.Add(this.yesOrNoBtn);
+            this.Controls.Add(this.predictionLabel);
+            this.Controls.Add(this.yesOrNoLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label yesOrNoLabel;
+        private System.Windows.Forms.Label predictionLabel;
+        private System.Windows.Forms.Button yesOrNoBtn;
+        private System.Windows.Forms.Button predictionBtn;
+    }
+}
+
---
+++ 8 лаба/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_лаба
+{
+    public partial class Form1 : Form
+    {
+        const double b = 4294967299;
+        const double m = 9223372036854775808;
+        double xNext = b;
+        double xBefore, xNow;
+        public Form1()
+        {
+            InitializeComponent();
+            for(int i = 0; i < 20; i++)
+            {
+                xBefore = xNext;
+                xNext = (b * xBefore) % m;
+                xNow = xNext / m;
+            }
+        }
+
+        private void predictionBtn_Click(object sender, EventArgs e)
+        {
+            xBefore = xNext;
+            xNext = (b * xBefore) % m;
+            xNow = xNext / m;
+            if (xNow < 0.05)
+                predictionLabel.Text = "Бесспорно";
+            else if (xNow >= 0.05 && xNow < 0.1)
+                predictionLabel.Text = "Предрешено";
+            else if (xNow >= 0.1 && xNow < 0.15)
+                predictionLabel.Text = "Никаких сомнений";
+            else if (xNow >= 0.15 && xNow < 0.2)
+                predictionLabel.Text = "Определённо да";
+            else if (xNow >= 0.2 && xNow < 0.25)
+                predictionLabel.Text = "Можешь быть уверен в этом";
+            else if (xNow >= 0.25 && xNow < 0.3)
+                predictionLabel.Text = "Мне кажется — «да»";
+            else if (xNow >= 0.3 && xNow < 0.35)
+                predictionLabel.Text = "Вероятнее всего";
+            else if (xNow >= 0.35 && xNow < 0.4)
+                predictionLabel.Text = "Хорошие перспективы";
+            else if (xNow >= 0.4 && xNow < 0.45)
+                predictionLabel.Text = "Знаки говорят — «да»";
+            else if (xNow >= 0.45 && xNow < 0.5)
+                predictionLabel.Text = "Да";
+            else if (xNow >= 0.5 && xNow < 0.55)
+                predictionLabel.Text = "Пока не ясно, попробуй снова";
+            else if (xNow >= 0.55 && xNow < 0.6)
+                predictionLabel.Text = "Спроси позже";
+            else if (xNow >= 0.6 && xNow < 0.65)
+                predictionLabel.Text = "Лучше не рассказывать";
+            else if (xNow >= 0.65 && xNow < 0.7)
+                predictionLabel.Text = "Сейчас нельзя предсказать";
+            else if (xNow >= 0.7 && xNow < 0.75)
+                predictionLabel.Text = "Сконцентрируйся и спроси опять";
+            else if (xNow >= 0.75 && xNow < 0.8)
+                predictionLabel.Text = "Даже не думай";
+            else if (xNow >= 0.8 && xNow < 0.85)
+                predictionLabel.Text = "Мой ответ — «нет»";
+            else if (xNow >= 0.85 && xNow < 0.9)
+                predictionLabel.Text = "По моим данным — «нет»";
+            else if (xNow >= 0.9 && xNow < 0.95)
+                predictionLabel.Text = "Перспективы не очень хорошие";
+            else if (xNow >= 0.95 && xNow < 1)
+                predictionLabel.Text = "Весьма сомнительно";
+        }
+
+        private void yesOrNoBtn_Click(object sender, EventArgs e)
+        {
+            xBefore = xNext;
+            xNext = (b * xBefore) % m;
+            xNow = xNext / m;
+            if (xNow < 0.5)
+                yesOrNoLabel.Text = "Нет";
+            else
+                yesOrNoLabel.Text = "Да";
+        }
+    }
+}
